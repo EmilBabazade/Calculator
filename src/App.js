7b90cdfc653fe0c6f092437f6ecd24cf01468379
emil <emilbabazade29@gmail.com>
2021-05-10 18:27:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createUseStyles} from 'react-jss'
+import 'jss-plugin-global'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = createUseStyles({
+    '@global': {
+        html: {
+            boxSizing: 'border-box'
+        },
+        'html, body, #root': {
+            height: '100%',
+            width: '100%',
+            margin: 0,
+            padding: 0
+        },
+        '*, *:before, *:after': {
+            boxSizing: 'inherit'
+        }
+    },
+})
+
+const App = () => {
+    return (
+        <div>
+      hello
+        </div>
+    )
 }
 
-export default App;
+export default App
