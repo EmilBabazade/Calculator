57e2214cafadeaa0892ee682bbdb5c437db826c8
emil <emilbabazade29@gmail.com>
2021-05-10 19:40:27 +0300

    calculator display screen styling

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {createUseStyles} from 'react-jss'
 import Pad from './Pad'
 
+const FONT_COLOR = 'white'
+
 const useStyles = createUseStyles({
     calculator: {
         padding: 5,
@@ -14,6 +16,27 @@ const useStyles = createUseStyles({
         fontFamily: 'Share Tech Mono, sans-serif',
         gridTemplateColumns: '1fr 1fr 1fr 1fr',
         gridTemplateRows: '1fr 1fr 1fr 1fr 1fr 1fr'
+    },
+    display: {
+        gridColumn: '1/7',
+        gridRow: '1',
+        margin: 0,
+        color: `${FONT_COLOR}`,
+        '& *': {
+            margin: 0,
+            padding: 0,
+            textAlign: 'end'
+        },
+        '& h3': {
+            color: '#b07200'
+        },
+        fontFamily: 'Geostar, sans-serif'
+    },
+    input: {
+        fontSize: 35
+    },
+    output: {
+        color: '#b07200'
     }
 })
 
@@ -24,30 +47,34 @@ const Calculator = () => {
     const operationBG = '#666666'
     const acBG = '#ac3939'
     const equalsBg = '#004466'
-    const color = '#f4f4f4'
     return(
         <div className={styles.calculator}>
-            <Pad bg={acBG} character='AC' column='1/3' row='2' color={color} />
-            <Pad bg={operationBG} character='/' column='3' row='2' color={color} />
-            <Pad bg={operationBG} character='X' column='4' row='2' color={color} />
+            <div className={styles.display}>
+                <div className={styles.output}>11 + 11</div>
+                <div className={styles.input}>22</div>
+            </div>
 
-            <Pad bg={numberBG} character='7' column='1' row='3' color={color} />
-            <Pad bg={numberBG} character='8' column='2' row='3' color={color} />
-            <Pad bg={numberBG} character='9' column='3' row='3' color={color} />
-            <Pad bg={operationBG} character='-' column='4' row='3' color={color} />
+            <Pad bg={acBG} character='AC' column='1/3' row='2' color={FONT_COLOR} />
+            <Pad bg={operationBG} character='/' column='3' row='2' color={FONT_COLOR} />
+            <Pad bg={operationBG} character='X' column='4' row='2' color={FONT_COLOR} />
+
+            <Pad bg={numberBG} character='7' column='1' row='3' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='8' column='2' row='3' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='9' column='3' row='3' color={FONT_COLOR} />
+            <Pad bg={operationBG} character='-' column='4' row='3' color={FONT_COLOR} />
             
-            <Pad bg={numberBG} character='4' column='1' row='4' color={color} />
-            <Pad bg={numberBG} character='5' column='2' row='4' color={color} />
-            <Pad bg={numberBG} character='6' column='3' row='4' color={color} />
-            <Pad bg={operationBG} character='+' column='4' row='4' color={color} />
+            <Pad bg={numberBG} character='4' column='1' row='4' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='5' column='2' row='4' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='6' column='3' row='4' color={FONT_COLOR} />
+            <Pad bg={operationBG} character='+' column='4' row='4' color={FONT_COLOR} />
 
-            <Pad bg={numberBG} character='1' column='1' row='5' color={color} />
-            <Pad bg={numberBG} character='2' column='2' row='5' color={color} />
-            <Pad bg={numberBG} character='3' column='3' row='5' color={color} />
-            <Pad bg={equalsBg} character='=' column='4' row='5/7' color={color} />
+            <Pad bg={numberBG} character='1' column='1' row='5' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='2' column='2' row='5' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='3' column='3' row='5' color={FONT_COLOR} />
+            <Pad bg={equalsBg} character='=' column='4' row='5/7' color={FONT_COLOR} />
 
-            <Pad bg={numberBG} character='0' column='1/3' row='6' color={color} />
-            <Pad bg={numberBG} character='.' column='3' row='6' color={color} />
+            <Pad bg={numberBG} character='0' column='1/3' row='6' color={FONT_COLOR} />
+            <Pad bg={numberBG} character='.' column='3' row='6' color={FONT_COLOR} />
         </div>
     )
 }
